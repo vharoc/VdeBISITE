81f41bf44c8b6c32236ea0a46f50a1f518cfc856
vharoc <vharoc@usal.es>
2023-03-10 01:08:31 +0100

    4oCommite

--- holaMundo.c
+++ holaMundo.c
@@ -4,5 +4,6 @@ int main(){
     printf("Hola mundo! :D\n");
     printf("Necesito que imprimas algo y\n");
     printf("despues subirte al repositorio\n");
+    printf("Ahora si, que se me olvido el checkout a la nueva branch\n");
     return 0;
 }
